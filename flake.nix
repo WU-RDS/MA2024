5878ba1b554f97d8d4e05df504f5b81bdaa8e5d4
Daniel Winkler <dw@dwinkler.org>
2025-09-22 12:36:36 +1000

    try build

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Dev env for MA";
+
+  inputs = {
+    nixpkgs.url = "github:rstats-on-nix/nixpkgs/2025-08-25";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    allSystems = [
+      "x86_64-linux"
+      "aarch64-darwin"
+    ];
+
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (
+        system:
+          f {
+            pkgs = import nixpkgs {inherit system;};
+          }
+      );
+  in {
+    devShells = forAllSystems (
+      {pkgs}: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            xquartz
+            (rWrapper.override
+              {
+                packages =
+                  import ./r-packages.nix {inherit pkgs;};
+              })
+          ];
+        };
+      }
+    );
+  };
+  nixConfig = {
+    extra-substituters = [
+      "https://rstats-on-nix.cachix.org"
+      "https://rde.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "rstats-on-nix.cachix.org-1:vdiiVgocg6WeJrODIqdprZRUrhi1JzhBnXv7aWI6+F0="
+      "rde.cachix.org-1:yRxQYM+69N/dVER6HNWRjsjytZnJVXLS/+t/LI9d1D4="
+    ];
+  };
+}
